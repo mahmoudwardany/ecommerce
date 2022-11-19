1ba7c54b414db1a4a7df9ee7148d167a576c8a67
Mahmoud Wardany <mahmoudwardany18@gmail.com>
2022-11-19 12:07:40 +0200

    React Redux Toolkit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
     },[])
   return (
   <> 
-  <Carousel>
+  <Carousel className='car'>
       <Carousel.Item>
         <img
           className="d-block w-100"
--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,27 +1,74 @@
-import React from 'react'
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import  './info.css'
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row'
 import { Link } from 'react-router-dom';
+
+import  './info.css'
 export default function Info() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
     <div className='info'>
-    <Form className='w-50 mx-md-auto mx-md-auto g-5'>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Your Name</Form.Label>
-      <Form.Control type="text" placeholder="Please Enter Your Name" required/>
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Your Number</Form.Label>
-      <Form.Control type="number" placeholder="Please Enter Your Number" required/>
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Your Adress</Form.Label>
-      <Form.Control type="text" placeholder="Please Enter Your Ardress" required/>
-    </Form.Group>
-    <Link className='nav-link'to='/greeting'><Button variant="success" type="submit">
-      Order Now
-    </Button> </Link> 
-  </Form></div>
+    <Form className='w-50 text-sm-center m-sm-auto bg-light' noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="12" className='text-sm-center m-sm-auto' controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name"
+            defaultValue="Mark"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Row>
+        <Form.Group as={Col} md="12" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last name"
+            defaultValue="Otto"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        </Row>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="12"className='text-sm-center m-sm-auto' controlId="validationCustom03">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="City" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Form.Group className="mb-3">
+        <Form.Check
+          required
+          label="Agree to terms and conditions"
+          feedback="You must agree before submitting."
+          feedbackType="invalid"
+        />
+      </Form.Group>
+<Link to='/greeting'className='nav-link m-auto text-sm-center' >
+  <Button type="submit"variant='success' >Order Now</Button>
+  </Link>    
+      
+    </Form>
+  
+  </div>
   )
 }
